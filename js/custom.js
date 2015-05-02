0465cfdb57353fb1fd0e0b839cfe702228f2d4ac
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-05-02 23:58:38 +0100

    Adding node web-server + FE pages

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $('#fullpage').fullpage({
+        anchors: ['home', 'personalStory', 'insights', 'demo', 'businessModel', 'futureSteps'],
+        slidesNavigation: true,
+    });
+});
